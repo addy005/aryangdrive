8f6c3fd113b236d1daaa3213c02a0cb5028d75ab
Trojanscript <64583763+addy005@users.noreply.github.com>
2022-12-10 20:04:08 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 DOWNLOAD_LOCATION = "./Downloads"
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "asddsadsd")
+BOT_TOKEN = os.environ.get("5878591373:AAFounJuwZNb-jSLCUYbr6z77BTZua6iWFI", "@addygd_bot")
 # BOT_TOKEN = "894269743:AAH3OAtoNulAMPwJg-Cp4zAGBxGazTlzn38"  #  @aryan_vbot
-APP_ID = int(os.environ.get("API_ID", "sdasdsd"))
-API_HASH = os.environ.get("API_HASH", "dsfdfsf")
+APP_ID = int(os.environ.get("20952490", "sdasdsd"))
+API_HASH = os.environ.get("4db4afe40e78ee4bd6112cdbf0e9ba05", "dsfdfsf")
 AUTH_GROUP = ""
 
 Creds_path = "creds"
